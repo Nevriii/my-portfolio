410f54e38b9f4aabb47d067c57e5dff38a221ca2
Nevri2 <vivintejada@gmail.com>
2024-11-18 23:26:17 +0800

    Updated Content 11/18/24-11:30pm

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import './Content.css';
+import BarongPic from '../Images/barong-pic-bg.jpg';
+import GradPic from '../Images/grad-pic-bg.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BarongPic from '../Images/barong-pic.png';
-import GradPic from '../Images/grad-pic.png';
 
-const Images = [BarongPic, GradPic];
+const images = [BarongPic, GradPic]; // Add more classes if you have more images
+
 
 const Content = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % Images.length);
-        }, 3000); // Change image every 2 seconds
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 3000); // Change image every 3 seconds
 
         return () => clearInterval(interval);
     }, []);
@@ -24,20 +25,19 @@ const Content = () => {
                     <span className="animated-word hello" data-word="Hello,">Hello,</span>
                     <span className="animated-word i" data-word="I">I</span>
                     <span className="animated-word am" data-word="am">am</span>
-                    <span className="animated-word name text-info" data-word="Ervin">Ervin</span>
+                    <span className="animated-word name text-info roboto-regular" data-word="Ervin">Ervin</span>
                 </h1>
                 <p className="text-content ms-md-2 text-center text-md-start roboto-light concept-three">
                     An aspiring <span className="animated-profession roboto-black text-success" data-word="Software Engineer">Software Engineer</span>
                 </p>
-
             </div>
             <div className="d-flex justify-content-center me-md-5">
                 <div className="slider">
-                    {Images.map((image, index) => (
+                    {images.map((image, index) => (
                         <div
                             className={`slider-image ${index === currentImageIndex ? 'active' : ''}`}
                             key={index}
-                            style={{ backgroundImage: `url(${image})` }}
+                            style={{backgroundImage: `url(${image})`}}
                         ></div>
                     ))}
                 </div>
