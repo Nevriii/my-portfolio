aa48902d8231a4084e96ed3c43858f26811003de
Nevri <vivintejada@gmail.com>
2024-12-29 17:14:49 +0800

    New Commits

---
+++ src/fifthContent/fifthContent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const FifthContent = () => {
+
+    return (
+        <div className="mainContent-container my-5">
+            <h1 className="display-4 roboto-regular" style={{ letterSpacing: "5px", paddingTop: "65px" }}>
+                Projects
+            </h1>
+            <div className="container d-flex flex-column flex-md-row align-items-start justify-content-center">
+                
+            </div>
+        </div>
+    );
+};
+
+export default FifthContent;
